bc0f09b85c06a4fa0404fee1d4388a898acb2c8b
chnandakumarreddy <chnandakumarreddy@gmail.com>
2016-03-09 00:11:38 -0800

    Added core functionality for Double Linked Lists and included unti test results

---
+++ DataStructures.DoubleLinkedList/DataStructures.DLinkedListTest/DoubleLinkedListTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DataStructures.DLinkedListTest
+{
+    [TestClass]
+    public class DoubleLinkedListTest
+    {
+
+        [TestMethod]
+        public void CanAddSingleNodeToDoubleLinkedList()
+        {
+            var list = new DataStructures.DoubleLinkedList<int>();
+            list.Push(10);
+            Assert.IsTrue(list.Count == 1);
+        }
+
+        [TestMethod]
+        public void CanAddMultipleNodesToDoubleLinkedList()
+        {
+            var list = new DataStructures.DoubleLinkedList<int>();
+            list.Push(10);
+            list.Push(20);
+            list.Push(30);
+            list.Push(40);
+            Assert.IsTrue(list.Count == 4);
+        }
+
+        [TestMethod]
+        public void CanRemoveNodeFromDoubleLinkedList()
+        {
+            var list = new DataStructures.DoubleLinkedList<int>();
+            list.Push(10);
+            list.Push(20);
+            list.Push(30);
+            var deletedItem = list.Pop();
+            Assert.IsTrue(deletedItem == 30 && list.Count == 2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void CanRemoveNodeFromEmptyDoubleLinkedList()
+        {
+            var list = new DataStructures.DoubleLinkedList<int>();
+            var deletedItem = list.Pop();
+        }
+
+        [TestMethod]
+        public void CanGetNodesFromDoubleLinkedList()
+        {
+            List<int> stackInput = new List<int>() { 10, 20, 30, 40, 50 };
+            var list = new DataStructures.DoubleLinkedList<int>();
+            foreach (var integerItem in stackInput)
+            {
+                list.Push(integerItem);
+            }
+            List<int> stackOutput = new List<int>();
+            foreach (var listItem in list)
+            {
+                stackOutput.Add(listItem);
+            }
+
+            CollectionAssert.AreEqual(stackInput, stackOutput);
+        }
+
+    }
+}
---
+++ DataStructures.DoubleLinkedList/DataStructures.DoubleLinkedList/DoubleLinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class DoubleLinkedList<T> : IStack<T>
+    {
+        private class Node
+        {
+            public T data { get; set; }
+            public Node prevNode { get; set; }
+            public Node nextNode { get; set; }
+
+            public Node(T newData)
+            {
+                data = newData;
+                prevNode = null;
+                nextNode = null;
+            }
+        }
+
+        private Node headNode { get; set; }
+        private Node currentNode { get; set; }
+        public int Count { get; private set; }
+
+        public void Push(T data)
+        {
+            Node newNode = new Node(data);
+            newNode.prevNode = null;
+            newNode.nextNode = null;
+
+            if (headNode == null && currentNode == null)
+            {
+                headNode = newNode;
+            }
+            else
+            {
+                currentNode.nextNode = newNode;
+                newNode.prevNode = currentNode;
+            }
+            currentNode = newNode;
+            Count++;
+            
+        }
+
+        public T Pop()
+        {
+            T data;
+            if (headNode == null)   //if list doesn't contain elements
+                throw new InvalidOperationException("Stack underflow");
+            if (headNode.nextNode == null && headNode.prevNode == null) //if list contains one element
+            {
+                data = headNode.data;
+                headNode = null;
+                return data;
+            }
+            //if list contains more than 1 elements
+            data = currentNode.data;            
+            currentNode = currentNode.prevNode; //set the current node as last but one node
+            currentNode.nextNode.prevNode = null;   //using the current node, set the prev node of deleted node to null
+            currentNode.nextNode.nextNode = null;   //using the current node, st the next node of deleted node to null
+            currentNode.nextNode = null;
+            Count--;
+            return data;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node tempNode = headNode;
+            while (tempNode != null)
+            {
+                yield return tempNode.data;
+                tempNode = tempNode.nextNode;
+            }
+        }
+
+    }
+}
